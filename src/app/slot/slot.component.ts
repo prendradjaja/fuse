c7b5a01275868a8def215f638cf77e8890ecf09c
Pandu Rendradjaja <3111845+prendradjaja@users.noreply.github.com>
2019-04-21 17:21:43 -0700

    <slot>: Add @Input fiveColor

--- src/app/slot/slot.component.ts
+++ src/app/slot/slot.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, Input, HostBinding } from "@angular/core";
 
 @Component({
   selector: "slot",
@@ -6,6 +6,10 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./slot.component.scss"]
 })
 export class SlotComponent implements OnInit {
+  @HostBinding("class.five-color")
+  @Input()
+  fiveColor: boolean;
+
   constructor() {}
 
   ngOnInit() {}
